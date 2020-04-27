37c14e8a837bd3d62554129016bfbd58c75a11b8
justin-sue <justincsue@gmail.com>
2020-04-26 18:14:28 -0700

    final save

--- src/components/Episode.test.js
+++ src/components/Episode.test.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
 import Episodes from "./Episodes";
 
-test("Episodes renders", () => {
-    render(<Episodes episodes={} />);
-})
+test("Episodes renders episodes", () => {
+    const mockData = [
+        {
+            id: 1234,
+            image: { medium: "url-one.jpg" },
+            name: "name",
+            season: 1,
+            number: 1,
+            summary: "<p>summary</p>",
+            runtime: 60,
+        },
+    ];
 
-test("Episodes renders with valid Missions", () => {
-    const mockData = [{}];
-    const { queryAllByText, rerender } = render(<Episodes missions={[]} />);
+    const { queryAllByText, rerender } = render(<Episodes episodes={[]} />);
 
-    let allEpisodes = queryAllByText(/mission/i);
+    let allEpisodes = queryAllByText(/episode/i);
+
     expect(allEpisodes).toHaveLength(0);
     expect(allEpisodes).toStrictEqual([]);
 
     rerender(<Episodes episodes={mockData} />);
-    allEpisodes = queryAllByText(/mission/i);
+    allEpisodes = queryAllByText(/episode/i);
     expect(allEpisodes).toHaveLength(1);
-    // expect(allEpisodes.toStrictEqual([]));
-})
+});
